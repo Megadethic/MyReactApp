5692707d61bd2d172034c4b73f03d22acd7089c5
Megadethic <ecclesiastes@tut.by>
2019-04-09 12:41:23 +0300

    use ref to manage an element of person component after rendering.

--- src/Persons/Person/Person.js
+++ src/Persons/Person/Person.js
@@ -1,15 +1,20 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useRef} from 'react';
 import classes from './Person.css';
 
 const person = props => {
+  const personRef = useRef(null);
+
   useEffect( () => {
     console.log("Person.useEffect");
+    personRef.current.focus();
   }, [props.name] );
 
-  return <div className={classes.Person}>
-    My name is {props.name}!<br/>
-    <input onChange={props.changeHandler}/>
-  </div>
+  return (
+      <div className={classes.Person}>
+          My name is {props.name}!<br/>
+          <input onChange={props.changeHandler} ref={personRef}/>
+      </div>
+  )
 };
 
 export default person;
